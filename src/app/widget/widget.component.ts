20ab6a8dd3af779a9937aaa3e79b5cdb7c338bd2
unknown <alexandr.ma@thewhitelabelagency.com>
2024-07-27 20:31:25 +0300

    Implemented normal- and reverse- order of converting on the widget, fixed some bugs as well

--- src/app/widget/widget.component.ts
+++ src/app/widget/widget.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input} from '@angular/core';
 import { Currency } from '../currency';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-widget',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './widget.component.html',
   styleUrl: './widget.component.css'
 })
@@ -18,12 +19,18 @@ export class WidgetComponent {
 
   onChange(e: any) {
     const target = e.target || e.srcElement || e.currentTarget,
-      idAttr = target.attributes.id,
-      theId = idAttr.nodeValue;
+      theParent = target.closest(".js-field-group"),
+      theParentId = theParent.attributes.id.nodeValue,
+      theInput = theParent.children[0],
+      theSelect = theParent.children[1];
 
-      if (e.target.value) {
-        let theCurrency = this.rates.filter((item: Currency) => (item['cc'] === this.selectFirstValue));    
-        this.inputSecondValue =  e.target.value * theCurrency[0]['rate'];
+      let theCurrency = this.rates.filter((item: Currency) => (item['cc'] === theSelect.value));
+
+      if (theCurrency && theParentId === 'field-group-1') {    
+        this.inputSecondValue =  +(theInput.value * theCurrency[0]['rate']).toFixed(3);
+      } else if (theParentId === 'field-group-2' && this.selectFirstValue) {
+        theCurrency = this.rates.filter((item: Currency) => (item['cc'] === this.selectFirstValue))
+        this.inputFirstValue =  +(this.inputSecondValue / theCurrency[0]['rate']).toFixed(3);
       }
   }
 }
